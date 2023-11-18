080aa311db85a76d2cadbbf7610f90473238f230
Angela Dai <zhjh.angela@gmail.com>
2023-11-18 18:31:46 -0500

    palm and ui merged

--- streamlit.py
+++ streamlit.py
@@ -14,8 +14,6 @@ model = models[0].name
 
 def call_api(age, sex, data):
     global model
-    sex = "male" if sex == "M" else "female"
-    
     prompt = """Pretend you are a cardiologist.You have an ECG interpreting machine learning model that classified a patient's ECG data and provided percentage likelihoods for each condition 1dAVb, RBBB, LBBB, SB, AF,  ST.
     
     Here are what the acronyms mean:
@@ -37,6 +35,8 @@ def call_api(age, sex, data):
     SB: """+ str(data[3]) +"""
     AF: """ + str(data[4]) + """
     ST: """+ str(data[5])
+    
+    print(prompt)
 
     completion = palm.generate_text(
         model=model,
@@ -61,21 +61,26 @@ uploaded_file = st.file_uploader("Upload your DICOM file (.dcm)")
 age = st.number_input("Patient age", min_value = 0, max_value = 120, value=60)
 sex  = st.selectbox("Patient sex", ["Male", "Female"])
 
-#ECG_image = st.image()
+labels = [ "1dAVb", "RBBB", "LBBB", "SB", "AF", "ST"]
 
-sample_array = {"1dAVb": 13.239485, "RBBB": 0.546861, "LBBB": 0.006619, "SB": 0.052661, "AF": 0.131343, "ST": 0.000431}
-
-st.bar_chart(data=sample_array, color="#e9a56b")
 #makes the prediction
 if st.button('Submit'):
-    # data = predict.make_prediction(path_to_hdf5 = "./automatic_ecg_diagnosis_master/data/ecg_tracings.hdf5", 
-                                #    path_to_model = "./automatic_ecg_diagnosis_master/model/model.hdf5")
-    data = np.array([0.17332049, 0.00245664, 0.000086,   0.00004635, 0.00205241, 0.00007277])
+    data = predict.make_prediction(path_to_hdf5 = "./automatic_ecg_diagnosis_master/data/ecg_tracings.hdf5", 
+                                   path_to_model = "./automatic_ecg_diagnosis_master/model/model.hdf5")
+    chart_data  = dict(zip(labels, data))
     st.text(data)
+    st.bar_chart(data=chart_data, color="#e9a56b")
     percent_data = np.round(data * 100.0, 2).astype(np.float16)
     st.text(percent_data)
     result = call_api(age, sex, data)
     st.text(result)
+
+#ECG_image = st.image()
+
+# sample_array = {"1dAVb": 13.239485, "RBBB": 0.546861, "LBBB": 0.006619, "SB": 0.052661, "AF": 0.131343, "ST": 0.000431}
+
+
+
     
     
 
