ec327a262cb0e440aecbf449aeb8f86c1c8b30d8
Aparna <71279313+bookmagyk@users.noreply.github.com>
2023-11-18 15:49:34 -0500

    Remove matplotlib stuff, clean up streamlit

--- streamlit.py
+++ streamlit.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from io import StringIO
-import matplotlib.pyplot as plt
 import numpy as np
 
 st.title('Automatic ECG diagnosis')
@@ -12,33 +11,6 @@ st.selectbox("Patient sex", ["Male", "Female"])
 
 #ECG_image = st.image()
 
-code = '''def hello():
-    print("Hello, Streamlit!")'''
-st.code(code, language='python')
-
 sample_array = {"1dAVb": 13.239485, "RBBB": 0.546861, "LBBB": 0.006619, "SB": 0.052661, "AF": 0.131343, "ST": 0.000431}
 
 st.bar_chart(data=sample_array, color="#e9a56b")
-
-plt.style.use('_mpl-gallery')
-
-# Names of items on the X-axis
-items = sample_array.keys()
-
-# Corresponding values on the Y-axis
-values = sample_array.values()
-
-# Creating the bar chart
-plt.bar(items, values)
-
-# Adding the title
-plt.title('Sample Bar Chart')
-
-# Labeling the X-axis
-plt.xlabel('Condition')
-
-# Labeling the Y-axis
-plt.ylabel('Percentage likelihood')
-
-# Display the plot
-plt.show()
