d736ee15db9a0bf13953bf8327b4b35de6eadb43
Angela Dai <zhjh.angela@gmail.com>
2023-11-18 14:39:29 -0500

    add rounding to percent

--- streamlit.py
+++ streamlit.py
@@ -1,14 +1,18 @@
 import streamlit as st
 from io import StringIO
+import numpy as np
 import automatic_ecg_diagnosis_master.predict as predict
 
-
+np.set_printoptions(suppress=True)
 code = '''def hello():
     print("Hello, Streamlit!")'''
 st.code(code, language='python')
 #makes the prediction
 if st.button('Submit'):
-    data = predict.make_prediction(path_to_hdf5="./automatic_ecg_diagnosis_master/data/ecg_tracings.hdf5", path_to_model = "./automatic_ecg_diagnosis_master/model/model.hdf5")
-    # right now just returns data
+    data = predict.make_prediction(path_to_hdf5 = "./automatic_ecg_diagnosis_master/data/ecg_tracings.hdf5", 
+                                   path_to_model = "./automatic_ecg_diagnosis_master/model/model.hdf5")
     st.text(data)
+    percent_data = np.round(data * 100.0, 2).astype(np.float16)
+    st.text(percent_data)
+
 
